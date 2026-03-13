808324caea151409db19247a41b97cde4089e54f
cadenannison <cadenannison@gmail.com>
2026-03-13 16:37:44 -0600

    setup for my test cases

--- server/src/test/java/dataacess/DataAccessTests.java
+++ server/src/test/java/dataacess/DataAccessTests.java
@@ -2,8 +2,14 @@ package dataacess;
 
 import dataaccess.DataAccessException;
 import dataaccess.MYSQLDataAccess;
+import model.AuthData;
+import model.GameData;
+import model.UserData;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collection;
 
 public class DataAccessTests {
 
@@ -17,6 +23,40 @@ public class DataAccessTests {
     void clearDataAccess() throws DataAccessException {
         dataAccess.clear();
     }
+
+    @Test
+    void clearTest() throws DataAccessException {
+
+    }
+
+    @Test
+    void createUser(UserData user) throws DataAccessException;
+
+    @Test
+    UserData getUser(String username) throws DataAccessException;
+
+    @Test
+    void createAuth(AuthData auth) throws DataAccessException;
+
+    @Test
+    AuthData getAuth(String authToken) throws DataAccessException;
+
+    @Test
+    void deleteAuthToken(String authToken) throws DataAccessException;
+
+    @Test
+    int createGame(String gameName) throws DataAccessException;
+
+    @Test
+    GameData getGame(int gameId) throws DataAccessException;
+
+    @Test
+    Collection<GameData> listGames() throws DataAccessException;
+
+    @Test
+    void joinGame(String username, String playerColor, int gameID) throws DataAccessException;
+
+
 
 
 

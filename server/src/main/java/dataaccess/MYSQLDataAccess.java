2e3d6656dfb188ace607e30dc05702d20437c178
cadenannison <cadenannison@gmail.com>
2026-03-13 19:04:42 -0600

    final changes

--- server/src/main/java/dataaccess/MYSQLDataAccess.java
+++ server/src/main/java/dataaccess/MYSQLDataAccess.java
@@ -24,9 +24,9 @@ public class MYSQLDataAccess implements DataAccess {
             try (PreparedStatement preparedStatement = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
                 for (int i = 0; i < params.length; i++) {
                     Object param = params[i];
-                    if (param instanceof String p)  preparedStatement.setString(i + 1, p);
-                    else if (param instanceof Integer p) preparedStatement.setInt(i + 1, p);
-                    else if (param == null) preparedStatement.setNull(i + 1, NULL);
+                    if (param instanceof String p)  preparedStatement.setString(i+1, p);
+                    else if (param instanceof Integer p) preparedStatement.setInt(i+1, p);
+                    else if (param == null) preparedStatement.setNull(i+1, NULL);
                 }
                 preparedStatement.executeUpdate();
                 ResultSet rs = preparedStatement.getGeneratedKeys();

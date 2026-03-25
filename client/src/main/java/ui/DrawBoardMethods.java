c0f6216fa87d7a88cde5a62062344ef67984db07
cadenannison <cadenannison@gmail.com>
2026-03-25 17:33:29 -0600

    added printletterRow helper and then added added initial functionality for printing board w/out returnPiece yet/

--- client/src/main/java/ui/DrawBoardMethods.java
+++ client/src/main/java/ui/DrawBoardMethods.java
@@ -1,9 +1,15 @@
 package ui;
 
 import chess.ChessBoard;
+import chess.ChessPiece;
+import chess.ChessPosition;
+
+import static ui.EscapeSequences.*;
 
 public class DrawBoardMethods {
     public static String draw(ChessBoard board, boolean whitePerspective){
+        var boardAsString = new StringBuilder();
+
         int rowStart;
         int rowEnd;
         int rowDirection;
@@ -18,7 +24,8 @@ public class DrawBoardMethods {
             colStart = 1;
             colEnd = 9;
             colDirection = 1;
-        } else {
+        }
+        else {
             rowStart = 1;
             rowEnd = 9;
             rowDirection = 1;
@@ -26,13 +33,44 @@ public class DrawBoardMethods {
             colEnd = 0;
             colDirection = -1;
         }
+        boardAsString.append(printLetterRow(colStart, colEnd, colDirection));
+
         for (int i = rowStart; i != rowEnd; i += rowDirection){
             for (int j = colStart; j != colEnd; j += colDirection){
+                if ((i+j) % 2 == 0){
+                    boardAsString.append(SET_BG_COLOR_WHITE);
+                }
+                else {
+                    boardAsString.append(SET_BG_COLOR_BLACK);
 
+                }
+                ChessPiece piece = board.getPiece(new ChessPosition(i, j));
+                if (piece == null){
+                    boardAsString.append(EMPTY);
+                }
+                else {
+                    boardAsString.append(returnPiece()); //need to get piece here
+                }
+                boardAsString.append(RESET_BG_COLOR);
             }
         }
     }
 
+    private static String printLetterRow(int colStart, int colEnd, int colDirection) {
+        var rowString = new StringBuilder();
+        rowString.append("   ");
+        for (int i = colStart; i != colEnd; i += colDirection) {
+            rowString.append((char)(i + 96)); //easier way to b able to print out the letters in
+            // the top and bottom rows
+            rowString.append("  ");
+        }
+        rowString.append("   \n");
+        return rowString.toString();
+    }
+
+    private static String returnPiece(ChessPiece piece) {
+
+    }
 
 
 }

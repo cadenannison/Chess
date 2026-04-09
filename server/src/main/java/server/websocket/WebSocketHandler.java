bde9bc0e6a6cdc67ddceee3d15f1088e8d29f87c
cadenannison <cadenannison@gmail.com>
2026-04-09 16:26:26 -0600

    finished makeMove and added additional checks and broadcasts to load game and notify everyone for moves, checks, and does error handling

--- server/src/main/java/server/websocket/WebSocketHandler.java
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -1,5 +1,6 @@
 package server.websocket;
 
+import chess.ChessGame;
 import com.google.gson.Gson;
 import dataaccess.DataAccess;
 import dataaccess.DataAccessException;
@@ -9,6 +10,7 @@ import io.javalin.websocket.WsConnectHandler;
 import io.javalin.websocket.WsMessageContext;
 import model.GameData;
 import org.eclipse.jetty.server.Authentication;
+import websocket.commands.MakeMoveCommand;
 import websocket.commands.UserGameCommand;
 import javax.swing.*;
 import java.io.IOException;
@@ -39,7 +41,10 @@ public class WebSocketHandler {
             UserGameCommand userGameCommand = new Gson().fromJson(ctx.message(), UserGameCommand.class);
             switch (userGameCommand.getCommandType()) {
                 case CONNECT -> gameConnect(ctx.session, userGameCommand);
-                case MAKE_MOVE -> makeMove(ctx.session, userGameCommand);
+                case MAKE_MOVE -> { //deserialize both parts of the object to get move positions
+                    MakeMoveCommand moveCommand = new Gson().fromJson(ctx.message(), MakeMoveCommand.class);
+                    makeMove(ctx.session, moveCommand);
+                }
                 case LEAVE -> leave(ctx.session, userGameCommand);
                 case RESIGN -> resign(ctx.session, userGameCommand);
             }
@@ -80,14 +85,81 @@ public class WebSocketHandler {
             connections.broadcast(gameId, session, notification);
         }
         catch (Exception e) {
-
+            errorSender(session, "Error: " + e.getMessage());
         }
     }
 
+    private void errorSender(Session session, String message) throws IOException {
+        var error = new websocket.messages.ErrorMessage(message);
+        if (session.isOpen()) {
+            session.getRemote().sendString(new Gson().toJson(error));
+        }
+    }
 
+    private void makeMove(Session session, MakeMoveCommand makeMoveCommand) throws IOException, DataAccessException {
+        var authorization = dataAccess.getAuth(makeMoveCommand.getAuthToken());
+        var gameData = dataAccess.getGame(makeMoveCommand.getGameID());
 
-    private void makeMove(Session session, UserGameCommand userGameCommand) throws IOException {
+        if (authorization == null) {
+            errorSender(session, "Error: Not Authorized");
+            return;
+        }
+        if (gameData == null) {
+            errorSender(session, "Error: wrong gameID");
+            return;
+        }
 
+        ChessGame game = gameData.game();
+        String username = authorization.username();
+        ChessGame.TeamColor playerColor = null;
+
+        if (username.equals(gameData.whiteUsername())) {
+            playerColor = ChessGame.TeamColor.WHITE;
+        }
+        else if (username.equals(gameData.blackUsername())) {
+            playerColor = ChessGame.TeamColor.BLACK;
+        }
+        if (playerColor == null) {
+            errorSender(session, "Error: Observers cant make moves");
+            return;
+        }
+
+        if (game.getTeamTurn() != playerColor) {
+            errorSender(session, "Error: Its not your turn");
+            return;
+        }
+        if (game.isOver() == true){
+            errorSender(session, "Error: game has already ended");
+            return;
+        }
+
+        try {
+            game.makeMove(makeMoveCommand.getMove());
+            dataAccess.updateGame(gameData);
+            connections.broadcast(gameData.gameID(), null, new LoadGameMessage(game));
+            String playerMove = username + " moved to " + makeMoveCommand.getMove().toString();
+            connections.broadcast(gameData.gameID(), session, new NotificationMessage(playerMove));
+            ChessGame.TeamColor opponent;
+            if (playerColor == ChessGame.TeamColor.WHITE) {
+                opponent = ChessGame.TeamColor.BLACK;
+            }
+            else {
+                opponent = ChessGame.TeamColor.WHITE;
+            }
+            if (game.isInCheckmate(opponent)) {
+                game.setGameOver(true);
+                dataAccess.updateGame(gameData);
+                connections.broadcast(gameData.gameID(), null,
+                        new NotificationMessage("Checkmate " + username + " wins the game!"));
+            }
+            else if (game.isInCheck(opponent)) {
+                connections.broadcast(gameData.gameID(), null,
+                        new NotificationMessage("Check :)"));
+            }
+        }
+        catch (Exception ex){
+            errorSender(session, "Error: " + ex.getMessage());
+        }
     }
 
     private void resign(Session session, UserGameCommand userGameCommand) throws IOException {

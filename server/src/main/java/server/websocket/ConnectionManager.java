26f90707d6b7f19abcfe34418f4f348d4dea6c9b
cadenannison <cadenannison@gmail.com>
2026-04-08 18:29:14 -0600

    added add and remove functions to connection manager

--- server/src/main/java/server/websocket/ConnectionManager.java
+++ server/src/main/java/server/websocket/ConnectionManager.java
@@ -1,19 +1,32 @@
 package server.websocket;
 
 import org.eclipse.jetty.websocket.api.Session;
-import webSocketMessages.Notification;
+
+import javax.management.Notification;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class ConnectionManager {
-    public final ConcurrentHashMap<Session, Session> connections = new ConcurrentHashMap<>();
+    public final ConcurrentHashMap<Integer, List<Session>> connections = new ConcurrentHashMap<>();
 
-    public void add(Session session) {
-        connections.put(session, session);
+    public void add(int gameId, Session session) {
+        if (connections.get(gameId) == null){ //if the list doesnt exist then make it and add the session.
+            List<Session> list = new ArrayList<>();
+            list.add(session);
+            connections.put(gameId, list); // then put that into the connections hashmap w the gameID
+        }
+        else {
+            connections.get(gameId).add(session);
+        }
     }
 
-    public void remove(Session session) {
-        connections.remove(session);
+    public void remove(int gameId, Session session) {
+        if (connections.get(gameId) == null){
+            return;
+        }
+        connections.get(gameId).remove(session);
     }
 
     public void broadcast(Session excludeSession, Notification notification) throws IOException {

5f5deb7413ad0a06b034780817796204df043f44
cadenannison <cadenannison@gmail.com>
2026-04-09 20:58:21 -0600

    gson object serializer

--- client/src/main/java/client/WebsocketFacade.java
+++ client/src/main/java/client/WebsocketFacade.java
@@ -1,6 +1,9 @@
 package client;
 
+import chess.ChessPiece;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializer;
 import jakarta.websocket.*;
 import websocket.commands.MakeMoveCommand;
 import websocket.commands.UserGameCommand;
@@ -13,6 +16,7 @@ public class WebsocketFacade extends Endpoint{
 
     Session session;
     NotificationHandler notificationHandler;
+    private final Gson gson = createGson();
 
     public WebsocketFacade(String url, NotificationHandler notificationHandler) throws Exception {
         try {
@@ -26,7 +30,7 @@ public class WebsocketFacade extends Endpoint{
             this.session.addMessageHandler(new MessageHandler.Whole<String>() {
                 @Override
                 public void onMessage(String message) {
-                    ServerMessage notification = new Gson().fromJson(message, ServerMessage.class);
+                    ServerMessage notification = gson.fromJson(message, ServerMessage.class);
                     notificationHandler.notify(notification);
                 }
             });
@@ -34,26 +38,37 @@ public class WebsocketFacade extends Endpoint{
             throw new Exception("Connection failed: " + ex.getMessage());
         }
     }
-
+    @Override
     public void onOpen(Session session, EndpointConfig endpointConfig) {
     }
 
     public void joinGame(String authToken, int gameID) throws IOException {
         var command = new UserGameCommand(UserGameCommand.CommandType.CONNECT, authToken, gameID);
-        this.session.getBasicRemote().sendText(new Gson().toJson(command));
+        this.session.getBasicRemote().sendText(gson.toJson(command));
     }
 
     public void leaveGame(String authToken, int gameID) throws IOException {
         var command = new UserGameCommand(UserGameCommand.CommandType.LEAVE, authToken, gameID);
-        this.session.getBasicRemote().sendText(new Gson().toJson(command));
+        this.session.getBasicRemote().sendText(gson.toJson(command));
     }
 
     public void resign(String authToken, int gameID) throws IOException {
         var command = new UserGameCommand(UserGameCommand.CommandType.RESIGN, authToken, gameID);
-        this.session.getBasicRemote().sendText(new Gson().toJson(command));
+        this.session.getBasicRemote().sendText(gson.toJson(command));
     }
 
     public void makeMove(MakeMoveCommand command) throws IOException {
-        this.session.getBasicRemote().sendText(new Gson().toJson(command));
+        this.session.getBasicRemote().sendText(gson.toJson(command));
+    }
+
+    private Gson createGson() {
+        GsonBuilder builder = new GsonBuilder();
+
+        builder.registerTypeAdapter(chess.ChessPiece.class,
+                (JsonDeserializer<ChessPiece>) (el, type, ctx) -> {
+            return ctx.deserialize(el, chess.ChessPiece.class);
+        });
+
+        return builder.create();
     }
 }
